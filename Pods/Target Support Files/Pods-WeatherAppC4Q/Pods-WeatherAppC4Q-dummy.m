dfe1077e37bbc0fe52e49e399ef0e23a05876022
Arsalan Ahadi <zarsalan19@gmail.com>
2017-01-07 14:11:28 -0500

    Initial Commit

---
+++ Pods/Target Support Files/Pods-WeatherAppC4Q/Pods-WeatherAppC4Q-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WeatherAppC4Q : NSObject
+@end
+@implementation PodsDummy_Pods_WeatherAppC4Q
+@end
---
+++ Pods/Target Support Files/Pods-WeatherAppC4Q/Pods-WeatherAppC4Q-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_WeatherAppC4QVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_WeatherAppC4QVersionString[];
+
